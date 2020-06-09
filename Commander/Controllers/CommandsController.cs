169ac7d615d2b4638b945f91b615e287dbb52650
wiktormuller <biznes.wiktor@gmail.com>
2020-06-09 13:44:58 +0200

    Add implementation to controller with hardly coupled implementation of our repository

--- Commander/Controllers/CommandsController.cs
+++ Commander/Controllers/CommandsController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Commander.Data;
+using Commander.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Commander.Controllers
@@ -11,6 +13,24 @@ namespace Commander.Controllers
     [ApiController]
     public class CommandsController : ControllerBase
     {
-        
+        private readonly MockCommanderRepo _repository = new MockCommanderRepo();
+
+        //GET api/commands
+        [HttpGet]
+        public ActionResult <IEnumerable<Command>> GetAllCommands()
+        {
+            var commands = _repository.GetAppCommands();
+
+            return Ok(commands);    //Ok 202
+        }
+
+        //GET api/commands/{id}
+        [HttpGet("{id}")]
+        public ActionResult <Command> GetCommandById(int id)    //The id comming from binding sources from [FromQuery]
+        {
+            var command = _repository.GetCommandById(id);
+
+            return Ok(command);
+        }
     }
 }
